17e1af438b0f778c8eb669f9bc0c81a63e66a618
Dasiy <dasiy@192.168.0.108>
2021-02-02 18:00:46 +0530

    Post Request

--- App.js
+++ App.js
@@ -1,31 +1,84 @@
-import React, { useEffect, useState } from 'react';
-import { ActivityIndicator, FlatList, SafeAreaView, Text, View} from 'react-native';
+import React, { useState } from 'react';
+import { Text,View, TextInput, StyleSheet, SafeAreaView, TouchableOpacity } from 'react-native';
 
 const App = () => {
-  const [isLoading, setLoading] = useState(true);
-  const [data, setData] = useState([]);
-  
-  useEffect(() => {
-    fetch('https://reactnative.dev/movies.json')
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+ const FetchAPI = async () => {
+    await fetch('https://dodbnb.herokuapp.com/api/v1/auth/register', {
+      method: 'POST',
+      headers: ({
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }),
+      body: JSON.stringify({
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        password: password
+      }),
+    })
       .then((response) => response.json())
-      .then((json) => setData(json.movies))
+      .then (response => console.log (response) )
+      
       .catch((error) => console.error(error))
-      .finally(() => setLoading(false));
-  }, []);
-
-
+  }
   return (
-    <SafeAreaView style = {{flex:1,flexDirection:"row"}}>
-      {isLoading ? <ActivityIndicator/> : (
-        <FlatList
-          data={data}
-          keyExtractor={({id},index) =>id}
-          renderItem={({ item }) => (
-           <Text style = {{fontSize:20,color:"midnightblue",margin:10,fontWeight:"bold",marginLeft:10}}>{item.title}, {item.releaseYear}</Text>
-          )}
-        />
-      )}
-    </SafeAreaView>  
-  );
+    <SafeAreaView>
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.inputText}
+          placeholder="Enter FirstName"
+          placeholderTextColor="black"
+          onChangeText={(text) => setFirstName(text)} />
+      </View>
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.inputText}
+          placeholder="Enter Last Name"
+          placeholderTextColor="black"
+          onChangeText={(text) => setLastName(text)} />
+      </View>
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.inputText}
+          placeholder="Enter Email or Username"
+          placeholderTextColor="black"
+          onChangeText={(text) => setEmail(text)} />
+      </View>
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.inputText}
+          placeholder="Enter Password"
+          placeholderTextColor="black"
+          onChangeText={(text) => setPassword(text)} />
+      </View>
+      <View style={{ top: 20, width: "33%", backgroundColor: "lightgreen", alignSelf: "center", alignItems: "center", borderRadius: 50, borderWidth: 1 }}>
+        <TouchableOpacity onPress = { ()=> FetchAPI()}>
+          <Text style={styles.signup}>Sign up</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  )
 }
-export default App;
+const styles = StyleSheet.create({
+  inputView: {
+    width: "80%",
+    margin: 25,
+    padding: 15,
+    backgroundColor: "white",
+    borderRadius: 25,
+    borderWidth: 2,
+    height: 50
+  },
+  signup: {
+    color: "black",
+    fontWeight: "bold",
+    fontSize: 25
+  },
+});
+
+export default App
